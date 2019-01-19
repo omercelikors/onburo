95b8dfb5a56705c788b91fc75ae346bbe9af6c8c
omercelikors <celikorsomerwebdev@gmail.com>
2019-01-19 21:02:05 +0300

    remove cache

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -14,7 +14,7 @@ class RolesAndPermissionsSeeder extends Seeder
     public function run()
     {
          // Reset cached roles and permissions
-         app()['cache']->forget('spatie.permission.cache');
+        
 
          $role1 = Role::create(['name' => 'admin']);
          $role2 = Role::create(['name' => 'recorder']);
