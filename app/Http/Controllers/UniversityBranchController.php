2789b9de5077123c7945bbc60b10ee160afdec33
omercelikors <celikorsomerwebdev@gmail.com>
2019-04-29 09:03:14 +0300

    desc in university branch

--- app/Http/Controllers/UniversityBranchController.php
+++ app/Http/Controllers/UniversityBranchController.php
@@ -14,7 +14,7 @@ class UniversityBranchController extends Controller
      */
     public function index()
     {
-        $branches=UniversityBranch::all()->orderBy('name', 'desc')->get();
+        $branches=UniversityBranch::orderBy('name', 'desc')->get();
         return view('system.branch.index',['branches'=>$branches]);
     }
 
