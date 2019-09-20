de7f9e3586651e19b73c69294609b0fa8f603098
Hayreddin Tüzel <hayreddin.tuzel@osf-commerce.com>
2019-09-20 21:51:13 +0300

    aday öğrenci temin sorumlusu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -69,6 +69,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('agency_info_show') }}">ACENTE</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('candidate_student_info_show') }}">ADAY ÖĞRENCİ</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">TEMİN</a>
                         <div class="dropdown-menu">
